d974c2fe75a02755240e598073d201381032a03e
Ken Morton Jr <kmorton15@gmail.com>
2020-03-02 23:53:31 -0500

    added description

--- components/Description.vue
+++ components/Description.vue
@@ -2,7 +2,9 @@
   <div>
     <h1 class="title  is-2">Description</h1>
     <p class="subtitle">
-      The description of this project will go here
+      This project was completed for a data viz course at WPI. Our goal was to
+      pick an existing data viz with a dataset, critique it, and improve on the
+      viz.
     </p>
   </div>
 </template>
