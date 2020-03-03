204ef30f6aac4b995ae02b6ce02022578f9dbe9d
Ken Morton Jr <kmorton15@gmail.com>
2020-03-02 23:45:21 -0500

    old viz description & sc

--- components/OldViz.vue
+++ components/OldViz.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <h1 class="title">Old Viz</h1>
+    <img src="oldViz.png" alt="Old Viz" />
     <h2 class="subtitle">
-      The old viz will go here
+      Here is a screenshot of the old viz. I decided to remix it because I knew
+      there was data for more years than just 2016, and because I felt that
+      there were better ways to represent the data than a bar chart.
     </h2>
   </div>
 </template>
